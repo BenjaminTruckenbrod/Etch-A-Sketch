5839064e31c358e271244eb2439ff415fa97897a
BenjaminTruckenbrod <btrucker94@gmail.com>
2022-05-23 16:21:31 -0400

    Finalize styling

--- js-file.js
+++ js-file.js
@@ -1,30 +1,46 @@
-const container = document.getElementById("container");
+let container = document.getElementById("container");
+const body = document.getElementById("body");
 
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
       let cell = document.createElement("div");
-      cell.innerText = (c + 1);
+      //cell.innerText = (c + 1);
       container.appendChild(cell).className = "grid-item";
     };
   };
   
-  makeRows(16, 16);
+  makeRows(16, 32);
 
 
 container.addEventListener("mouseover", function( event ) {
     // highlight the mouseover target
-    event.target.style.backgroundColor = "orange";
-  
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    //event.target.style.backgroundColor = "#" + randomColor;
+    event.target.style.backgroundColor = 'orange';
     // reset the color after a short delay
     setTimeout(function() {
       event.target.style.backgroundColor = "";
     //   event.target.style.transition = 'background-color 1s';
-    }, 500);
+    }, 150);
   }, false);
 
-  const dimenstions = document.getElementById("dimensions")
-dimenstions.addEventListener("click", function(event){
-    alert("sup");
-}); 
+function dimensions() {
+    let dimensions = prompt("Enter the dimensions", "16x16");
+    arr = dimensions.split('x')
+    if (arr[0] < 100|| arr[1] < 100){
+    const grid_items = document.getElementsByClassName("grid-item");
+    for (var i = grid_items.length - 1; i >= 0; --i) {
+        grid_items[i].remove();
+      }
+    
+    if (dimensions != null) {
+        
+        makeRows(parseInt(arr[0]),parseInt(arr[1]))
+    }
+    }
+    else{
+        
+    }
+}
